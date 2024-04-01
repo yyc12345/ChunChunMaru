fff9f1b7d4858836bf52ad99163c2f67455305d5
yyc12345 <yyc12321@outlook.com>
2024-04-01 10:52:45 +0800

    finish leaves gen

--- modifier.py
+++ modifier.py
@@ -31,37 +31,149 @@ class TreeType(enum.Enum):
     Overworld = 'log'
     Nether = 'stem'
     Bamboo = 'block'
-def tree_block(ctx: common.McContext, name: str, color_tuple: TreeBlockColors_t, tree_type: TreeType) -> None:
+def tree_block(ctx: common.McContext, tree_name: str, color_tuple: TreeBlockColors_t, tree_type: TreeType) -> None:
     """
     color_tuple = tuple(log_color, log_top_color, stripped_log_color, stripped_log_top_color, planks_color), 
     None if this log do not need process.
     """
     # get wood tail
-    wood_tail: str = tree_type.value
+    tree_tail: str = tree_type.value
 
     # log
     if color_tuple[0] is not None:
-        drawer.border_block_texture(ctx, f'{name}_{wood_tail}', color_tuple[0])
+        drawer.border_block_texture(ctx, f'{tree_name}_{tree_tail}', color_tuple[0])
     # log top
     if color_tuple[1] is not None:
-        drawer.border_block_texture(ctx, f'{name}_{wood_tail}_top', color_tuple[1])
+        drawer.border_block_texture(ctx, f'{tree_name}_{tree_tail}_top', color_tuple[1])
     # stripped log
     if color_tuple[2] is not None:
-        drawer.border_block_texture(ctx, f'stripped_{name}_{wood_tail}', color_tuple[2])
+        drawer.border_block_texture(ctx, f'stripped_{tree_name}_{tree_tail}', color_tuple[2])
     # stripped log top
     if color_tuple[3] is not None:
-        drawer.border_block_texture(ctx, f'stripped_{name}_{wood_tail}_top', color_tuple[3])
+        drawer.border_block_texture(ctx, f'stripped_{tree_name}_{tree_tail}_top', color_tuple[3])
     # planks
     if color_tuple[4] is not None:
-        drawer.border_block_texture(ctx, f'{name}_planks', color_tuple[4])
+        drawer.border_block_texture(ctx, f'{tree_name}_planks', color_tuple[4])
 
 def prepare_tree_leaves(ctx: common.McContext) -> None:
     # generate 2 leaves overlay
     drawer.generate_leaves_level(ctx)
     drawer.generate_leaves_persistent(ctx)
 
-    # generate leaves overlay used models
-    # todo...
+    # generate general leaves overlay models
+    # every single levaes level should inherit this model
+    # prepare model data first
+    payload: typing.Any = {
+        "parent": "block/block",
+        "textures": {
+            "particle": "#all"
+        },
+        "elements": [{
+            "from": [4, 4, -0.1],
+            "to": [12, 12, 16.1],
+            "faces": {
+                "north": {
+                    "texture": "#all",
+                    "cullface": "north"
+                },
+                "south": {
+                    "texture": "#all",
+                    "cullface": "south"
+                }
+            }
+        }, {
+            "from": [-0.1, 4, 4],
+            "to": [16.1, 12, 12],
+            "faces": {
+                "west": {
+                    "texture": "#all",
+                    "cullface": "west"
+                },
+                "east": {
+                    "texture": "#all",
+                    "cullface": "east"
+                }
+            }
+        }, {
+            "from": [4, -0.1, 4],
+            "to": [12, 16.1, 12],
+            "faces": {
+                "down": {
+                    "texture": "#all",
+                    "cullface": "down"
+                },
+                "up": {
+                    "texture": "#all",
+                    "cullface": "up"
+                }
+            }
+        }]
+    }
+    # write into files for level and presistent
+    ctx.write_model('leaves_level', payload)
+    ctx.write_model('leaves_persistent', payload)
 
-def tree_leaves(ctx: common.McContext, name: str) -> None:
-    pass
+    # write 1-7 tree levels model
+    for i in range(7):
+        # adjust to 1 based
+        i += 1
+        payload = {
+            "parent": "minecraft:block/leaves_level",
+            "textures": {
+                "all": f"minecraft:block/leaves_level_{i}"
+            }
+        }
+        ctx.write_model(f'leaves_level_{i}', payload)
+    # write persistent
+    for is_persistent in (True, False):
+        payload = {
+            "parent": "minecraft:block/leaves_persistent",
+            "textures": {
+                "all": f"minecraft:block/leaves_persistent_{'on' if is_persistent else 'off'}"
+            }
+        }
+        ctx.write_model(f"leaves_persistent_{'on' if is_persistent else 'off'}", payload)
+
+def tree_leaves(ctx: common.McContext, tree_name: str, color: str | None) -> None:
+    # build name
+    name: str = f'{tree_name}_leaves'
+    # write blockstates
+    blockstate_payload: typing.Any = {
+        "multipart": [	
+            {   "apply": { "model": f"minecraft:block/{name}" } },
+            {   "when": { "distance": "1" },
+                "apply": { "model": "minecraft:block/leaves_level_1" }
+            },
+            {   "when": { "distance": "2" },
+                "apply": { "model": "minecraft:block/leaves_level_2" }
+            },
+            {   "when": { "distance": "3" },
+                "apply": { "model": "minecraft:block/leaves_level_3" }
+            },
+            {   "when": { "distance": "4" },
+                "apply": { "model": "minecraft:block/leaves_level_4" }
+            },
+            {   "when": { "distance": "5" },
+                "apply": { "model": "minecraft:block/leaves_level_5" }
+            },
+            {   "when": { "distance": "6" },
+                "apply": { "model": "minecraft:block/leaves_level_6" }
+            },
+            {   "when": { "distance": "7" },
+                "apply": { "model": "minecraft:block/leaves_level_7" }
+            },	
+            {   "when": { "persistent": "true" },
+                "apply": { "model": "minecraft:block/leaves_persistent_on" }
+            },
+            {   "when": { "persistent": "false" },
+                "apply": { "model": "minecraft:block/leaves_persistent_off" }
+            }
+        ]
+    }
+    ctx.write_blockstate(name, blockstate_payload)
+
+    # we use vanilla tree model (actually combined with our level overlay)
+    # so no need to change model
+    # adding border for tree texture is enough
+    if color is not None:
+        drawer.border_block_texture(ctx, name, color)
--- utils.py
+++ utils.py
@@ -1,13 +1,6 @@
 import drawer, modifier, common
 import typing
 
-_OVERWORLD_WOOD_TYPE: tuple[str, ...] = (
-    'oak', 'spruce', 'birch', 'jungle', 'acacia', 'dark_oak', 'mangrove', 'cherry', 'bamboo'
-)
-_NETHER_WOOD_TYPE: tuple[str, ...] = (
-    'crimson', 'warped'
-)
-
 def _iterate_copper_type() -> typing.Iterator[str]:
     for waxed in ('', 'waxed_'):
         for oxidezed_level in ('', 'exposed_', 'weathered_', 'oxidized_'):
@@ -24,6 +17,7 @@ def proc_ore(ctx: common.McContext) -> None:
     modifier.overworld_ore(ctx, 'lapis_ore', '#446fdc')
     modifier.overworld_ore(ctx, 'redstone_ore', '#ff0000')
     # nether
+    drawer.border_block_texture(ctx, 'gilded_blackstone', '#a5650e')
     drawer.border_block_texture(ctx, 'nether_gold_ore', '#fcee4b')
     drawer.border_block_texture(ctx, 'nether_quartz_ore', '#d4caba')
     # nether ancient debris need colorful border
@@ -33,11 +27,23 @@ def proc_ore(ctx: common.McContext) -> None:
     drawer.border_block_texture(ctx, 'raw_copper_block', '#a65947')
     drawer.border_block_texture(ctx, 'raw_gold_block', '#fcee4b')
     drawer.border_block_texture(ctx, 'raw_iron_block', '#d8af93')
+    # ore block
+    # only coal block need border, other blocks is easy to distinguish
+    drawer.border_block_texture(ctx, 'coal_block', '#292727')
+    # amethyst
+    drawer.border_block_texture(ctx, 'amethyst_block', '#5d3a9a')
+    drawer.border_block_texture(ctx, 'budding_amethyst', '#be8e59')
 
 def proc_redstone(ctx: common.McContext) -> None:
     # door showcase
     # todo: add copper door when updating to 1.20.3
-    for door_type in _OVERWORLD_WOOD_TYPE + _NETHER_WOOD_TYPE + ('bamboo', 'iron'):
+    door_types: tuple[str, ...] = (
+        'oak', 'spruce', 'birch', 'jungle', 'acacia', 'dark_oak', 'mangrove', 'cherry',
+        'bamboo', 
+        'crimson', 'warped',
+        'iron',
+    )
+    for door_type in door_types:
         modifier.redstone_door(ctx, door_type + '_door')
 
 def proc_common(ctx: common.McContext) -> None:
@@ -45,31 +51,83 @@ def proc_common(ctx: common.McContext) -> None:
     # log (+-stripped) (原木)
     # wood (+-stripped) (木头) use the same texture with log so no need to process it
     # planks (木板)
-    def call_with_check(ctx: common.McContext, name: str, color_tuple: modifier.TreeBlockColors_t) -> None:
-        """
-        color_tuple = tuple(log_color, log_top_color, stripped_log_color, stripped_log_top_color, planks_color)
-        """
-        # check name
-        tree_type: modifier.TreeType
-        if name in _OVERWORLD_WOOD_TYPE:
-            if name == 'bamboo': tree_type = modifier.TreeType.Bamboo
-            else: tree_type = modifier.TreeType.Overworld
-        elif name in _NETHER_WOOD_TYPE: tree_type = modifier.TreeType.Nether
-        else: raise Exception('invalid tree type')
-        # call real function
-        modifier.tree_block(ctx, name, color_tuple, tree_type)
-    call_with_check(ctx, 'oak', ('#382b18', None, '#846a3a', None, '#67502c'))
-    call_with_check(ctx, 'spruce', ('#553a1f', None, '#544525', None, '#553a1f'))
-    call_with_check(ctx, 'birch', ('#aea6a4', None, '#9d8754', None, '#907e58'))
-    call_with_check(ctx, 'jungle', ('#635820', None, '#977c45', None, '#68462f'))
-    call_with_check(ctx, 'acacia', ('#5b554d', None, '#8f4c2a', None, '#7b4024'))
-    call_with_check(ctx, 'dark_oak', ('#584428', '#53381a', '#32281a', None, '#291a0c'))
-    call_with_check(ctx, 'mangrove', ('#3c2f23', '#3c2f23', '#632122', None, '#5d1c1e'))
-    call_with_check(ctx, 'cherry', ('#1b0f16', None, '#c07974', None, '#cd8580'))
-    call_with_check(ctx, 'bamboo', ('#5a6627', '#505c23', '#615526', '#615526', '#615526'))
-    call_with_check(ctx, 'crimson', ('#7b0000', '#442131', '#6a2640', None, '#3f1e2d'))
-    call_with_check(ctx, 'warped', ('#16615b', '#442131', '#2e8578', None, '#113835'))
+    # color_tuple = tuple(log_color, log_top_color, stripped_log_color, stripped_log_top_color, planks_color)
+    modifier.tree_block(ctx, 'oak', ('#382b18', None, '#846a3a', None, '#67502c'), modifier.TreeType.Overworld)
+    modifier.tree_block(ctx, 'spruce', ('#553a1f', None, '#544525', None, '#553a1f'), modifier.TreeType.Overworld)
+    modifier.tree_block(ctx, 'birch', ('#aea6a4', None, '#9d8754', None, '#907e58'), modifier.TreeType.Overworld)
+    modifier.tree_block(ctx, 'jungle', ('#635820', None, '#977c45', None, '#68462f'), modifier.TreeType.Overworld)
+    modifier.tree_block(ctx, 'acacia', ('#5b554d', None, '#8f4c2a', None, '#7b4024'), modifier.TreeType.Overworld)
+    modifier.tree_block(ctx, 'dark_oak', ('#584428', '#53381a', '#32281a', None, '#291a0c'), modifier.TreeType.Overworld)
+    modifier.tree_block(ctx, 'mangrove', ('#3c2f23', '#3c2f23', '#632122', None, '#5d1c1e'), modifier.TreeType.Overworld)
+    modifier.tree_block(ctx, 'cherry', ('#1b0f16', None, '#c07974', None, '#cd8580'), modifier.TreeType.Overworld)
+    modifier.tree_block(ctx, 'bamboo', ('#5a6627', '#505c23', '#615526', '#615526', '#615526'), modifier.TreeType.Bamboo)
+    modifier.tree_block(ctx, 'crimson', ('#7b0000', '#442131', '#6a2640', None, '#3f1e2d'), modifier.TreeType.Nether)
+    modifier.tree_block(ctx, 'warped', ('#16615b', '#442131', '#2e8578', None, '#113835'), modifier.TreeType.Nether)
 
     # tree leaves
     # prepare leaves first
     modifier.prepare_tree_leaves(ctx)
+    # process tree leaves on by one (there is no bamboo leaves)
+    modifier.tree_leaves(ctx, 'oak', '#686468')
+    modifier.tree_leaves(ctx, 'spruce', None)
+    modifier.tree_leaves(ctx, 'birch', '#706d70')
+    modifier.tree_leaves(ctx, 'jungle', None)
+    modifier.tree_leaves(ctx, 'acacia', None)
+    modifier.tree_leaves(ctx, 'dark_oak', None)
+    modifier.tree_leaves(ctx, 'mangrove', None)
+    modifier.tree_leaves(ctx, 'cherry', '#738726')
+    # there is no bamboo leaves, so no need to process it
+    # there is a extra leaves type: azalea_leaves (杜鹃)
+    modifier.tree_leaves(ctx, 'azalea', None)
+    # nether leaves (nether wart + shroomlight) do not have levels, so we just border it.
+    drawer.border_block_texture(ctx, 'nether_wart_block', '#3e0808')
+    drawer.border_block_texture(ctx, 'warped_wart_block', '#13585d')
+    drawer.border_block_texture(ctx, 'shroomlight', '#b62f09')
+
+    # overworld block
+    # stone, cobblestone, deepslate, cobbled deepslate
+    drawer.border_block_texture(ctx, 'stone', '#5d5c5c')
+    drawer.border_block_texture(ctx, 'cobblestone', '#4e4d4d')
+    drawer.border_block_texture(ctx, 'deepslate', '#202028')
+    drawer.border_block_texture(ctx, 'cobbled_deepslate', '#2b2b30')
+    # todo...
+    # sand, gravel, dirt, grass
+    drawer.border_block_texture(ctx, 'sand', '#cab57d')
+    drawer.border_block_texture(ctx, 'clay', '#8a919d')
+    drawer.border_block_texture(ctx, 'grass_block_top', '#828282')
+    drawer.border_block_texture(ctx, 'dirt', '#664730')
+    drawer.border_block_texture(ctx, 'dirt_path_top', '#775f33')
+    drawer.border_block_texture(ctx, 'coarse_dirt', '#676767')
+    drawer.border_block_texture(ctx, 'gravel', '#5d5555')
+    # todo...
+    # overworld stone variants
+    drawer.border_block_texture(ctx, 'diorite', '#757373')
+    drawer.border_block_texture(ctx, 'andesite', '#514f4f')
+    drawer.border_block_texture(ctx, 'granite', '#5f4034')
+    drawer.border_block_texture(ctx, 'tuff', '#4d5046')
+    drawer.border_block_texture(ctx, 'calcite', '#b3b3b3')
+    # obsidian
+    drawer.border_block_texture(ctx, 'obsidian', '#3b2754')
+    # ice, packed ice, blue ice, snow
+    drawer.border_block_texture(ctx, 'ice', '#7daafb')
+    drawer.border_block_texture(ctx, 'packed_ice', '#6794e8')
+    drawer.border_block_texture(ctx, 'blue_ice', '#4c8ffb')
+    drawer.border_block_texture(ctx, 'snow', '#b5e1e1')
+    # moss, azalea
+    drawer.border_block_texture(ctx, 'moss_block', '#384926')
+    drawer.border_block_texture(ctx, 'azalea_top', '#384926')
+    
+
+
+    # nether block
+    drawer.border_block_texture(ctx, 'magma', '#421616')
+    drawer.border_block_texture(ctx, 'soul_sand', '#3a2d25')
+    drawer.border_block_texture(ctx, 'soul_soil', '#3b2e25')
+    drawer.border_block_texture(ctx, 'blackstone', '#3c3947')
+    drawer.border_block_texture(ctx, 'blackstone_top', '#3c3947')
+    # todo...
+
+    # end block
+    drawer.border_block_texture(ctx, 'end_stone', '#aba579')
+    # todo...
+
